39229c033b8a15c7eff405289e304a64d53b5f06
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2020-11-24 09:42:51 +0100

    main alumnos terminado

--- src/MainAlumnos.java
+++ src/MainAlumnos.java
@@ -15,76 +15,129 @@ public class MainAlumnos {
         ArrayList <Alumno> alumnosCiencias=new ArrayList<Alumno>();
         ArrayList <Alumno> alumnosSinAsignatura=new ArrayList<Alumno>();
 
-        System.out.println("Introduce el número de alumnos que quieres crear");
-        int nAlumnos = sc.nextInt();
-        sc.nextLine();
-
-        for(int i=0;i<nAlumnos;i++) {
-            ArrayList<Integer> notas = new ArrayList<Integer>();
-            Alumno alumno = new Alumno();
-            System.out.println("Introduce un nombre");
-            String nombre = sc.nextLine();
-            alumno.setNombre(nombre);
-            System.out.println("Introduce la edad");
-            int edad = sc.nextInt();
+            //Crear alumnos
+            System.out.println("Introduce el número de alumnos que quieres crear");
+            int nAlumnos = sc.nextInt();
             sc.nextLine();
-            alumno.setEdad(edad);
-            int id=i;
-            alumno.setId(id);
-            for(int j=0;j<3;j++){
-                int random = (int) (Math.random() * 10 + 1);
-                notas.add(random);
+
+            for (int i = 0; i < nAlumnos; i++) {
+                ArrayList<Integer> notas = new ArrayList<Integer>();
+                Alumno alumno = new Alumno();
+                System.out.println("Introduce un nombre");
+                String nombre = sc.nextLine();
+                alumno.setNombre(nombre);
+                System.out.println("Introduce la edad");
+                int edad = sc.nextInt();
+                sc.nextLine();
+                alumno.setEdad(edad);
+                int id = i;
+                alumno.setId(id);
+                for (int j = 0; j < 3; j++) {
+                    int random = (int) (Math.random() * 10 + 1);
+                    notas.add(random);
+                }
+                alumno.setNotas(notas);
+                System.out.println("¿A qué asignatura quieres asignarlo?\n" +
+                        "1- Matemáticas\n" +
+                        "2- Castellano\n" +
+                        "3- Ciencias");
+                int opcAsignatura = sc.nextInt();
+                sc.nextLine();
+                switch (opcAsignatura) {
+                    case 1:
+                        alumnosMates.add(alumno);
+                        break;
+                    case 2:
+                        alumnosCastellano.add(alumno);
+                        break;
+                    case 3:
+                        alumnosCiencias.add(alumno);
+                        break;
+                    default:
+                        alumnosSinAsignatura.add(alumno);
+                        break;
+                }
+                alumnos.add(alumno);
             }
-            alumno.setNotas(notas);
-            System.out.println("¿A qué asignatura quieres asignarlo?\n" +
+            Asignatura matematicas = new Asignatura("Matematicas", alumnosMates);
+            Asignatura castellano = new Asignatura("Castellano", alumnosCastellano);
+            Asignatura ciencias = new Asignatura("Ciencias", alumnosCiencias);
+
+            //Matricular alumnos
+            for (int i = 0; i < alumnos.size(); i++) {
+                System.out.println(alumnos.get(i).getNombre());
+                System.out.println(alumnos.get(i).getId());
+                System.out.println("");
+            }
+            System.out.println("¿Que alumno quieres matricular?(Introduce su ID)");
+
+            int opcAlumno = sc.nextInt();
+            sc.nextLine();
+            for (int i = 0; i < alumnos.size(); i++) {
+                Alumno alumno = new Alumno();
+                if (alumnos.get(i).getId() == opcAlumno) {
+                    alumno = alumnos.get(i);
+                    System.out.println("¿A qué asignatura lo quieres matricular?\n" +
+                            "1- Matemáticas\n" +
+                            "2- Castellano\n" +
+                            "3- Ciencias");
+                    int opcAsignatura = sc.nextInt();
+                    sc.nextLine();
+                    switch (opcAsignatura) {
+                        case 1:
+                            alumnosMates.add(alumno);
+                            break;
+                        case 2:
+                            alumnosCastellano.add(alumno);
+                            break;
+                        case 3:
+                            alumnosCiencias.add(alumno);
+                            break;
+                    }
+                }
+            }
+
+            //Dar de baja alumnos
+            System.out.println("¿En qué asignatura quieres dar de baja?\n" +
                     "1- Matemáticas\n" +
                     "2- Castellano\n" +
                     "3- Ciencias");
             int opcAsignatura = sc.nextInt();
             sc.nextLine();
-            switch (opcAsignatura){
-                case 1:
-                    alumnosMates.add(alumno);
-                    break;
-                case 2:
-                    alumnosCastellano.add(alumno);
-                    break;
-                case 3:
-                    alumnosCiencias.add(alumno);
-                    break;
-                default:
-                    alumnosSinAsignatura.add(alumno);
-                    break;
+            Asignatura asignatura = new Asignatura();
+            if (opcAsignatura == 1) {
+                asignatura = matematicas;
+            } else if (opcAsignatura == 2) {
+                asignatura = castellano;
+            } else if (opcAsignatura == 3) {
+                asignatura = ciencias;
+            } else {
+                System.out.println("No has seleccionado ninguna asignatura");
             }
-            alumnos.add(alumno);
-        }
 
-        //Matricular alumnos
-        System.out.println("¿Que alumno quieres matricular?");
-        for(int i=0;i<alumnos.size();i++){
-            System.out.println(alumnos.get(i).getNombre());
-            System.out.println(alumnos.get(i).getId());
+            asignatura.printAlumnos();
+            System.out.println("Selecciona que alumno quieres dar de baja (Introduce el ID)");
+            int opcAlumnoBaja = sc.nextInt();
+            sc.nextLine();
+            for (int i = 0; i < asignatura.getAlumnos().size(); i++) {
+                if (asignatura.getAlumnos().get(i).getId() == opcAlumno) {
+                    asignatura.bajaAlumno(asignatura.getAlumnos().get(i));
+                }
+            }
+
+            //Mostrar todos los alumnos
+            matematicas.printAsignatura();
             System.out.println("");
+            matematicas.printAlumnos();
+            System.out.println("");
+            castellano.printAsignatura();
+            System.out.println("");
+            castellano.printAlumnos();
+            System.out.println("");
+            ciencias.printAsignatura();
+            System.out.println("");
+            ciencias.printAlumnos();
         }
-
-
-
-        Asignatura matematicas = new Asignatura("Matematicas",alumnosMates);
-        Asignatura castellano = new Asignatura("Castellano",alumnosCastellano);
-        Asignatura ciencias = new Asignatura("Ciencias",alumnosCiencias);
-
-        matematicas.printAsignatura();
-        System.out.println("");
-        matematicas.printAlumnos();
-        System.out.println("");
-        castellano.printAsignatura();
-        System.out.println("");
-        castellano.printAlumnos();
-        System.out.println("");
-        ciencias.printAsignatura();
-        System.out.println("");
-        ciencias.printAlumnos();
-
 
 
 
@@ -95,5 +148,5 @@ public class MainAlumnos {
         }
 
 
-    }
+
 
